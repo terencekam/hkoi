9ef29797404d69653bd18ff36881474cba6d9b2b
Terence <64822471+terencekam@users.noreply.github.com>
2022-04-29 10:19:43 +0800

    Create D100.java

---
+++ java/D100.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class main {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        int i2 = scanner.nextInt();
+        scanner.close();
+        System.out.println(i+i2);
+    }
+}
